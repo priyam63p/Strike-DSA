0c143b4f0d4489687713db8dbe861ce17aed9e73
Priyam Patra <priyampatra63@gmail.com>
2025-12-03 18:49:59 +0530

    Add files via upload

---
+++ L1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>>
+using namespace std;
+int main(){
+    // int a=10;
+    // double height=5.11;
+    // cout<<"Height is:"<<height;
+    // char grade='A';
+    // bool isYes=true;
+    // string name="Priyam";
+    // cout<<isYes<<endl;
+    // cout<<grade<<endl;
+    // cout<<name<<endl;
+    // cout<<name.length()<<endl;
+    // std::cout<<a;
+    // cout<<a;
+
+    // string firstName="Priyam";
+    // string lastName="Patra";
+    // string fullName=firstName+" "+lastName;
+    // cout<<fullName<<endl;
+
+
+    // string name;
+    // cout<<"Enter your Name:";
+    // cin>>name;
+    // cout<<"Your name is "<<name;
+
+    string name;
+    cout<<"Enter your Name:";
+    getline(cin,name);
+    cout<<"Your name is:"<<name;
+}
