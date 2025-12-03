b4fbec301aaf41702d39fb0bc1c57c696474c42f
Priyam Patra <priyampatra63@gmail.com>
2025-12-03 20:01:31 +0530

    Add files via upload

---
+++ L2.cpp
@@ -0,0 +1,223 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a=10,b=2;
+    // cout<<a+b<<endl;
+    // cout<<a-b<<endl;
+    // cout<<a*b<<endl;
+    // cout<<a/b<<endl;
+
+    // int marks1=90;
+    // int marks2=24;
+    // int sum=marks1+marks2;
+    // cout<<sum;
+
+    // int x=10;
+    // x+=8;
+    // cout<<x<<endl;
+    // x-=8;
+    // cout<<x<<endl;
+    // x*=8;
+    // cout<<x<<endl;
+    // x/=5;
+    // cout<<x<<endl;
+
+    // int a=10,b=20;
+
+    // cout<<(a>b);
+
+    // cout<<true&&true;
+
+    // int x=5;
+    // cout<<x++<<endl;
+    // cout<<x;
+
+    // int score=0;
+    // cout<<score++<<endl;
+    // cout<<score<<endl;
+    // cout<<score++<<endl;
+    // cout<<score<<endl;
+
+    // string name;
+    // int age;
+
+    // cout << "Enter your name: ";
+    // cin >> name;
+
+    // cout << "Enter your age: ";
+    // cin >> age;
+
+    // cout << "\nHello " << name << "!" << endl;
+    // cout << "You are " << age << " years old." << endl;
+
+    // return 0;
+
+    // int age = 19;
+    // if (age >= 19)
+    // {
+    //     cout << "You are adult";
+    // }
+    // else
+    // {
+    //     cout << "You are not adult";
+    // }
+
+    // int marks = 85;
+
+    // if (marks >= 90)
+    // {
+    //     cout << "Grade: A";
+    // }
+    // else if (marks >= 80)
+    // {
+    //     cout << "Grade: B";
+    // }
+    // else if (marks >= 70)
+    // {
+    //     cout << "Grade: C";
+    // }
+    // else if (marks >= 60)
+    // {
+    //     cout << "Grade: D";
+    // }
+    // else
+    // {
+    //     cout << "Grade: F";
+    // }
+
+    // int age=19;
+    // bool handliscense=false;
+
+    // if(age>=19){
+    //     if(handliscense){
+    //         cout<<"You are a driver";
+    //     }
+    //     else{
+    //         cout<<"You are not a driver";
+    //     }
+    // }
+    // else{
+    //     cout<<"You are not a driver";
+    // }
+
+    // int num;
+    // cout<<"Enter a number:";
+    // cin>>num;
+    // if(num%2==0){
+    //     cout<<"The entered number is even";
+    // }
+    // else{
+    //     cout<<"The entered number is odd";
+    // }
+
+    // int num1,num2;
+    // cout<<"Enter first number:";
+    // cin>>num1;
+    // cout<<"Enter second number:";
+    // cin>>num2;
+    // if(num1>num2){
+    //     cout<<num1<<" is greater than "<<num2;
+    // }
+    // else{
+    //     cout<<num2<<" is greater than "<<num1;
+    // }
+
+    // int num;
+    // cout<<"Enter a number:";
+    // cin>>num;
+    // if(num>0){
+    //     cout<<"Entered number is Positive";
+
+    // }else if(num==0){
+    //     cout<<"Entered number is zero";
+
+    // }
+
+    // else{
+    //     cout<<"Entered number is Negative";
+    // }
+
+    // int marks;
+
+    // cout << "Enter your marks (0-100): ";
+    // cin >> marks;
+
+    // if(marks > 100 || marks < 0) {
+    //     cout << "Invalid marks!";
+    // } else if(marks >= 90) {
+    //     cout << "Grade: A (Excellent)";
+    // } else if(marks >= 80) {
+    //     cout << "Grade: B (Very Good)";
+    // } else if(marks >= 70) {
+    //     cout << "Grade: C (Good)";
+    // } else if(marks >= 60) {
+    //     cout << "Grade: D (Satisfactory)";
+    // } else {
+    //     cout << "Grade: F (Fail)";
+    // }
+
+    // double num1, num2;
+    // char op;
+
+    // cout << "Enter first number: ";
+    // cin >> num1;
+
+    // cout << "Enter operator (+, -, *, /): ";
+    // cin >> op;
+
+    // cout << "Enter second number: ";
+    // cin >> num2;
+
+    // if (op == '+')
+    // {
+    //     cout << "Result: " << (num1 + num2);
+    // }
+    // else if (op == '-')
+    // {
+    //     cout << "Result: " << (num1 - num2);
+    // }
+    // else if (op == '*')
+    // {
+    //     cout << "Result: " << (num1 * num2);
+    // }
+    // else if (op == '/')
+    // {
+    //     if (num2 != 0)
+    //     {
+    //         cout << "Result: " << (num1 / num2);
+    //     }
+    //     else
+    //     {
+    //         cout << "Cannot divide by zero!";
+    //     }
+    // }
+    // else
+    // {
+    //     cout << "Invalid operator!";
+    // }
+
+    // for(int i=0;i<=10;i++){
+    //     cout<<i<<endl;
+    // }
+
+    int sum = 0;
+    for (int i = 0; i <= 3; i++)
+    {
+        sum = sum + i;
+        /* code */
+    }
+    cout << "The sum is " << sum;
+
+    // for(int i = 10; i >= 1; i--) {
+    // cout << i << " ";
+
+    // int table = 5;
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     int multiplies = table * i;
+    //     cout<<table << "X"<<i<<"="<< multiplies<<endl;
+    // }
+
+    return 0;
+}
