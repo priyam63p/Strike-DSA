3847d75cd1ce01ee3641cd650b09794411c9c2ab
Priyam Patra <priyampatra63@gmail.com>
2025-12-05 17:55:04 +0530

    Add files via upload

---
+++ L3.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // *****
+    // *****
+    // *****
+    // *****
+    // *****
+
+    // for (int j = 1; j <= 6; j++)
+    // {
+    //     for (int i = 0; i <= 5; i++)
+    //     {
+    //         cout << '*';
+    //     }
+    //     cout<<endl;
+    // }
+
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+    // for (int j = 1; j <= 6; j++)
+    // {
+    //     for (int i = 1; i <= j; i++)
+    //     {
+    //         cout << '*';
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for (int j = 5; j >= 1; j--)
+    // {
+    //     for (int i = 1; i <= j; i++)
+    //     {
+    //         cout << '*';
+    //     }
+    //     cout << endl;
+    // }
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+
+//   for (int j = 5; j >= 1; j--) wrong
+//     {
+//         for (int i = 1; i <= j; i++)
+//         {
+//             cout << ' ';
+//         }
+//         cout << '*'<<endl;
+//     }
+
+
+// for(int i = 1; i <= 5; i++) {
+   
+//     for(int j = 1; j <= 5-i; j++) {
+//         cout << " ";
+//     }
+    
+//     for(int j = 1; j <= i; j++) {
+//         cout << "*";
+//     }
+//     cout << endl;
+// }
+
+
+
+// 1 2 3 4 5
+// 6 7 8 9 10
+// 11 12 13 14 15
+// 16 17 18 19 20
+// 21 22 23 24 25 
+    
+    // for (int i = 1; i <=21; i=i+5)
+    // {
+    //     for (int j = i; j <=i+4; j++)
+    //     {
+    //         cout<<j<<' ';
+    //     }
+    //     cout<<endl;
+    // }
+
+    return 0;
+}
