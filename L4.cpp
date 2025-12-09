9c4354c8476e7452224e7d5501c1322dc96a48e4
Priyam Patra <priyampatra63@gmail.com>
2025-12-09 23:40:31 +0530

    Add files via upload

---
+++ L4.cpp
@@ -0,0 +1,191 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // A
+    // AB
+    // ABC
+    // ABCD
+    // ABCDE
+
+    // for (char j = 'A'; j <= 'E'; j++)
+    // {
+    //     for (char i = 'A'; i <= j; i++)
+    //     {
+    //         cout << i;
+    //     }
+    //     cout << endl;
+    // }
+
+    // A
+    // BB
+    // CCC
+    // DDDD
+    // EEEEE
+    // FFFFFF
+
+    // for (int j = 0; j <= 4; j++)
+
+    // {
+    //     char num = 'A' + j;
+    //     for (int i = 0; i <= j; i++)
+
+    //     {
+    //         cout << num;
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     cout<<" ";
+
+    // }
+    // for (int i = 1; i <=1; i++)
+    // {
+    //     cout<<i;
+
+    // }
+
+    // cout<<endl;
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     cout<<" ";
+
+    // }
+    // for (int i = 1; i <=2; i++)
+    // {
+    //     cout<<i;
+
+    // }
+
+    //   for (int i = 1; i >=1; i--)
+    // {
+    //     cout<<i;
+
+    // }
+
+    //   cout<<endl;
+
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     cout<<" ";
+
+    // }
+    // for (int i = 1; i <=3; i++)
+    // {
+    //     cout<<i;
+
+    // }
+
+    //   for (int i = 2; i >=1; i--)
+    // {
+    //     cout<<i;
+
+    // }
+
+    //    cout<<endl;
+
+    // for (int i = 0; i < 1; i++)
+    // {
+    //     cout<<" ";
+
+    // }
+    // for (int i = 1; i <=4; i++)
+    // {
+    //     cout<<i;
+
+    // }
+
+    //   for (int i = 3; i >=1; i--)
+    // {
+    //     cout<<i;
+
+    // }
+
+    //  cout<<endl;
+
+    // for (int i = 0; i < 0; i++)
+    // {
+    //     cout<<" ";
+
+    // }
+    // for (int i = 1; i <=5; i++)
+    // {
+    //     cout<<i;
+
+    // }
+
+    //   for (int i = 4; i >=1; i--)
+    // {
+    //     cout<<i;
+
+    // }
+
+    for (int i = 0; i < (5-i); i++)
+    {
+        cout<<" ";
+
+    }
+    for (int i = 1; i <=5; i++)
+    {
+        cout<<i;
+
+    }
+
+      for (int i = 4; i >=1; i--)
+    {
+        cout<<i;
+
+    }
+
+    // PRIME NUMBER
+
+    // int num;
+    // cout << "Enter a number:" << endl;
+    // cin >> num;
+    // if (num < 2)
+    // {
+    //     cout << "Not a prime number";
+    // }
+
+    // for (int i = 2; i < (num - 1); i++)
+    // {
+    //     if (num % i == 0)
+    //     {
+    //         cout << "Not Prime Number";
+    //         return 0;
+    //     }
+    // }
+
+    // cout << "Prime Number";
+
+
+
+    //WHILE LOOP
+    //  int i=0;
+    //  while (i<=10){
+    //     cout<<i<<" ";
+    //     i++;
+    //  }
+
+    // SUM OF DIGITS
+
+    // int num;
+    // cout << "Enter a number:" << endl;
+    // cin >> num;
+    // int sum = 0;
+
+    // int i = 0;
+    // while (num)
+    // {
+    //     int rem = num % 10;
+    //     sum = sum + rem;
+    //     num /= 10;
+    // }
+    // cout << sum;
+
+    // return 0;
+}
