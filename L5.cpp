0b0ab614337950327af10ae1baa2c4adcfc17623
Priyam Patra <priyampatra63@gmail.com>
2025-12-13 19:20:22 +0530

    Add files via upload

---
+++ L5.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // REVERSE THE DIGIT
+    //  int num,ans=0;
+    //  cout<<"Enter a number:"<<endl;
+    //  cin>>num;
+    //  for (int i = 1; i <=num; i++)
+    //  {
+    //      int rem=num%10;
+    //      num=num/10;
+    //      ans=ans*10+rem ;
+
+    // }
+    // cout<<ans;
+
+    // FACTORIAL
+
+    // int num;
+    // cout<<"Enter a number:"<<endl;
+    // cin>>num;
+    // // long long fact=1;
+
+    // int fact=1;
+    // for (int i = 1; i <=num; i++)
+    // {
+    //      fact=fact*i;
+
+    // }
+    // cout<<fact;
+
+    // SQRT
+    int num,ans;
+    cout << "Enter a number:" << endl;
+    cin >> num;
+    for (int i = 1; i*i <=num; i++)
+    {
+        ans=i;
+        
+    }
+    cout<<ans;
+    
+
+
+
+
+
+
+
+
+    // int num, finalAns = 0;
+    // num = finalAns;
+    // cout << "Enter a number:" << endl;
+    // cin >> num;
+    // for (int i = 1; i < num; i++)
+    // {
+    //     int rem = num % 2;
+    //     int num = num / 2;
+    //     int finalAns = finalAns * 10 + rem;
+    // }
+    // cout << finalAns;
+    //  for (int i = 1; i <=num; i++)
+    //  {
+    //      int rem=num%10;
+    //      num=num/10;
+    //      int ans=ans*10+rem ;
+
+    // }
+    // cout<<ans;
+
+    return 0;
+}
